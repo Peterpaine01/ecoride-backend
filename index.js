f46b582fe6a8c2d7443cda101deb703010780e85
Fanny Carlier <75118010+Peterpaine01@users.noreply.github.com>
2025-02-11 00:19:17 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+require("dotenv").config();
+
+const fileUpload = require("express-fileupload");
+const cors = require("cors");
+require("./config/mysql");
+require("./config/mongodb");
+require("./config/cloudinary");
+
+// create server
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT || 5000;
+
+// -------- SET UP ----------
+
+app.all("*", (req, res) => {
+  res.status(404).json({ message: "This route does not exist" });
+});
+
+app.get("/", (req, res) => {
+  res.send("Server Node.js running !");
+});
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
