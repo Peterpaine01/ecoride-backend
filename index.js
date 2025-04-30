7d37e0c123a3ae4ac8a52211a1311e95cd8ad425
Fanny CARLIER <75118010+Peterpaine01@users.noreply.github.com>
2025-04-30 04:43:38 +0200

    use openroutes

--- index.js
+++ index.js
@@ -1,66 +1,67 @@
-const express = require("express");
-require("dotenv").config();
-const axios = require("axios");
-const cron = require("node-cron");
-const { updateDailyStatistics } = require("./services/statisticsService");
+const express = require("express")
+require("dotenv").config()
+const axios = require("axios")
+const cron = require("node-cron")
+const { updateDailyStatistics } = require("./services/statisticsService")
 
-const cors = require("cors");
-require("./config/mysql");
-require("./config/mongodb");
-require("./config/cloudinary");
+const cors = require("cors")
+require("./config/mysql")
+require("./config/mongodb")
+require("./config/cloudinary")
 
 // create server
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+const app = express()
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // use routes
-app.use(require("./routes/userRoutes"));
-app.use(require("./routes/authRoutes"));
-app.use(require("./routes/carRoutes"));
-app.use(require("./routes/rideRoutes"));
-app.use(require("./routes/bookingRoutes"));
-app.use(require("./routes/reviewRoutes"));
-app.use(require("./routes/adminRoutes"));
+app.use(require("./routes/userRoutes"))
+app.use(require("./routes/authRoutes"))
+app.use(require("./routes/carRoutes"))
+app.use(require("./routes/rideRoutes"))
+app.use(require("./routes/bookingRoutes"))
+app.use(require("./routes/reviewRoutes"))
+app.use(require("./routes/adminRoutes"))
+app.use(require("./routes/openRoutes"))
 
-const PORT = process.env.PORT || 5000;
-console.log(`Server running on port: ${PORT}`);
+const PORT = process.env.PORT || 5000
+console.log(`Server running on port: ${PORT}`)
 
 // -------- SET UP ----------
 
 // Exécuter la fonction tous les jours à minuit UTC
 cron.schedule("0 0 * * *", async () => {
-  console.log("⏳ Running updateDailyStatistics...");
+  console.log("⏳ Running updateDailyStatistics...")
   try {
-    await updateDailyStatistics();
-    console.log("✅ Statistics updated !");
+    await updateDailyStatistics()
+    console.log("✅ Statistics updated !")
   } catch (error) {
-    console.error("❌ Error updating statistics : " + error);
+    console.error("❌ Error updating statistics : " + error)
   }
-});
+})
 
 // Find Ip public Northflank
 axios
   .get("https://api64.ipify.org?format=json")
   .then((response) => {
-    console.log(`🌍 IP publique du serveur: ${response.data.ip}`);
+    console.log(`🌍 IP publique du serveur: ${response.data.ip}`)
   })
   .catch((error) => {
-    console.error("❌ Impossible de récupérer l'IP publique", error);
-  });
+    console.error("❌ Impossible de récupérer l'IP publique", error)
+  })
 
 app.get("/", (req, res) => {
   res
     .status(200)
-    .json({ message: "Server Node.js running! Welcome to Ecoride project!" });
-});
+    .json({ message: "Server Node.js running! Welcome to Ecoride project!" })
+})
 
 app.all("*", (req, res) => {
-  res.status(404).json({ message: "This route does not exist" });
-});
+  res.status(404).json({ message: "This route does not exist" })
+})
 
 app.listen(PORT, "0.0.0.0", async () => {
-  console.log(`Server running on port: ${PORT}`);
+  console.log(`Server running on port: ${PORT}`)
   // await connectToDatabase();
-});
+})
