77d88d0889538c63e8c07e040810f983e975d557
Fanny CARLIER <75118010+Peterpaine01@users.noreply.github.com>
2025-04-30 05:02:04 +0200

    cors options

--- index.js
+++ index.js
@@ -9,9 +9,15 @@ require("./config/mysql")
 require("./config/mongodb")
 require("./config/cloudinary")
 
+const corsOptions = {
+  origin: "https://ecoride-mobility.netlify.app",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}
+
 // create server
 const app = express()
-app.use(cors())
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
